610da37bf700d93532a7afdbd2d1aeef11158520
Sean Murphy <filmmakersean@fastmail.net>
2019-08-03 23:51:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,22 @@
 class Song
   
-  attr_accessor :name, :artist
+  attr_accessor :name
+  
+  attr_writer :artist
   
   @@all = []
   
   def initialize(name)
     @name = name
     @@all << self
+  end
+  
+  def artist_name
+    @artist
+  end
+  
+  def self.all
+    @@all
   end
   
 end
